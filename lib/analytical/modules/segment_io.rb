2014b3ddf44504ac3b53e79dc0c38701746e62ec
Bargains4Business <mikel@bargains4business.com.au>
2015-02-04 15:24:16 +1000

    Update segment_io tracking snippet

--- lib/analytical/modules/segment_io.rb
+++ lib/analytical/modules/segment_io.rb
@@ -13,7 +13,10 @@ module Analytical
           js = <<-HTML
           <!-- Analytical Init: Segment.io -->
           <script type="text/javascript">
-            window.analytics=window.analytics||[];window.analytics.load=function(apiKey){var script=document.createElement('script');script.type='text/javascript';script.async=true;script.src=('https:'===document.location.protocol?'https://':'http://')+'d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/'+apiKey+'/analytics.min.js';var firstScript=document.getElementsByTagName('script')[0];firstScript.parentNode.insertBefore(script,firstScript);var methodFactory=function(type){return function(){window.analytics.push([type].concat(Array.prototype.slice.call(arguments,0)))}};var methods=['identify','track','trackLink','trackForm','trackClick','trackSubmit','pageview','ab','alias','ready'];for(var i=0;i<methods.length;i++){window.analytics[methods[i]]=methodFactory(methods[i])}};window.analytics.load('#{options[:key]}');
+            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
+            analytics.load("#{options[:key]}");
+            analytics.page()
+            }}();
           </script>
           HTML
           js
